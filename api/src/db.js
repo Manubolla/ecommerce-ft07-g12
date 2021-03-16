fb0efb432e249430aa7b7f084b616e17c52f8fa2
Manuel Bolla Agrelo <manubolla17@gmail.com>
2021-03-16 16:50:29 -0300

    herokuProblem

--- api/src/db.js
+++ api/src/db.js
@@ -6,10 +6,13 @@ const ProductImages = require('./models/ProdImages');
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 const sequelize = new Sequelize(
- `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?sslmode=require`,
+ `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
  {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  ssl: {
+    rejectUnauthorized: false
+  }
  }
 );
 const basename = path.basename(__filename);

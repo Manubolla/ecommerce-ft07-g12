83236143e4be5bdaf2ca81309fbe6d78ea1b12d7
Manuel Bolla Agrelo <manubolla17@gmail.com>
2021-06-29 13:24:03 -0300

    cors

--- api/src/app.js
+++ api/src/app.js
@@ -16,16 +16,13 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods' ,'GET, PUT, DELETE, POST, PATCH'); // ESTO PERMITE HACER ESTE TIPO DE PETICIONES
- res.header(
-  'Access-Control-Allow-Headers',
-  'Origin, X-Requested-With, Content-Type, Accept, Authorization'
- );
- next();
-});
+server.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,  Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+  });
 
 server.use(passport.initialize());
 server.all('*', function(req, res, next){
